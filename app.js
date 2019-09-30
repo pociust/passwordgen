b5eed3ea2220afb0bd351e282c19cfe20a5f59a7
Tom Pocius <poci14@gmail.com>
2019-09-29 21:18:34 -0500

    created 2 varibles with special char and #, special char with random index generator

--- app.js
+++ app.js
@@ -11,10 +11,20 @@ genPw.addEventListener('click', showAlert);
 //create an alert to set char limit 8-128
 
 
-//create alert to include special char
+//create alert to include special char 
+var specialGen = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '+', '=', '{', '[', '}', ']', '<', '>', '?', '+'];
+
+console.log(Math.floor(Math.random() * specialGen.length));
+
+console.log(specialGen[Math.floor(Math.random() * specialGen.length)]);
+
 
 //create an alert to include numbers
+var numGen = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
+for (var i = 0; i < numGen.length; i++){
+  // Math.floor(Math.random() * 10);
+}
 //create an alert to include upper case
 
 //create an alert to include lower case
