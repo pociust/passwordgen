d1619258d3f3bf5e145fdb7b6389fc0568ce87e6
Tom Pocius <poci14@gmail.com>
2019-10-01 12:49:22 -0500

    fixed button check on html, added concat for js, problem with it being 2x the length due to concat, will fix

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ document.getElementById("genPw").addEventListener('click', generatePassword);
 const numberGen = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const specialGen = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '+', '=', '{', '[', '}', ']', '<', '>', '?', '+'];
 function generatePassword(){
-  if (document.getElementById("numberSwitch").checked){
+  if (document.getElementById("numberSwitch").checked && document.getElementById("symbolSwitch").checked){
     pw ='';
     for (i = 0; i < document.getElementById("passwordSlide").value; i++){
       //select random specailgen
-      pw = pw.concat(numberGen[Math.floor(Math.random() * numberGen.length)]);
+      pw = pw.concat(numberGen[Math.floor(Math.random() * numberGen.length)]).concat(specialGen[Math.floor(Math.random() * specialGen.length)]);
     };
     console.log(pw);
     document.getElementById("pwBox").innerHTML = pw;
