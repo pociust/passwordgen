76f5b809e341ba0123f0f440c7fcaf988c3702b9
Tom Pocius <poci14@gmail.com>
2019-09-30 20:08:11 -0500

    added toggler js with if statement to only show password when toggled on

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-let pw = '';
-
-
-
-
-//create an alert to set char limit 8-128
 
 //create alert to include special char 
 
-const specialGen = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '+', '=', '{', '[', '}', ']', '<', '>', '?', '+'];
 
-function generatePassword(){
-  pw ='';
-  for (i = 0; i < document.getElementById("passwordSlide").value; i++){
-    //select random specailgen
-    pw = pw.concat(specialGen[Math.floor(Math.random() * specialGen.length)]);
-  };
-  document.getElementById("pwBox").innerHTML = pw;
-  console.log(pw);
-};
+// function generatePassword(){
+//   pw ='';
+//   for (i = 0; i < document.getElementById("passwordSlide").value; i++){
+//     //select random specailgen
+//     pw = pw.concat(specialGen[Math.floor(Math.random() * specialGen.length)]);
+//   };
+//   document.getElementById("pwBox").innerHTML = pw;
+//   console.log(pw);
+// };
 
+// document.getElementById("genPw").addEventListener('click', generatePassword);
 document.getElementById("genPw").addEventListener('click', generatePassword);
 
-
-//create an alert to include numbers
 
 const numberGen = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const specialGen = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '+', '=', '{', '[', '}', ']', '<', '>', '?', '+'];
 function generatePassword(){
-  pw ='';
-  for (i = 0; i < document.getElementById("passwordSlide").value; i++){
-    //select random specailgen
-    pw = pw.concat(numberGen[Math.floor(Math.random() * numberGen.length)]);
+  if (document.getElementById("numberSwitch").checked){
+    pw ='';
+    for (i = 0; i < document.getElementById("passwordSlide").value; i++){
+      //select random specailgen
+      pw = pw.concat(numberGen[Math.floor(Math.random() * numberGen.length)]);
+    };
+    console.log(pw);
+    document.getElementById("pwBox").innerHTML = pw;
   };
-  document.getElementById("pwBox").innerHTML = pw;
-  console.log(pw);
 };
 
-document.getElementById("genPw").addEventListener('click', generatePassword);
+
+
 
 //create an alert to include upper case
 
