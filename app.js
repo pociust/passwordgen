25a311ec00b291730f4423c6d27b0c7581f883a3
Tom Pocius <poci14@gmail.com>
2019-10-02 12:59:56 -0500

    added cap letters in js, started with label for the box for password in html and some styling css

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 
-//create alert to incluner('click', generatePassword);
 
 let basicPassword = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']; 
 const numberGen = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const specialGen = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '+', '=', '{', '[', '}', ']', '<', '>', '?', '+'];
+const capLetterGen = ['Q', 'W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
+
 function generatePassword(){
   let tempPassword = basicPassword
   if (document.getElementById("numberSwitch").checked){
     tempPassword = tempPassword.concat(numberGen);
-    console.log('number' + tempPassword);
   };
   
   if (document.getElementById("symbolSwitch").checked){
     tempPassword = tempPassword.concat(specialGen);
-    console.log('symbol' + tempPassword);
   };
-  console.log(tempPassword);
+
+  if (document.getElementById("capitalizationSwitch").checked){
+    tempPassword = tempPassword.concat(capLetterGen);
+  };
     pw ='';
     for (i = 0; i < document.getElementById("passwordSlide").value; i++){
       //select random specailgen
@@ -26,15 +28,4 @@ function generatePassword(){
 
 document.getElementById("genPw").addEventListener('click', generatePassword);
 
-
-
-//create an alert to include upper case
-
-//create an alert to include lower case
-
-
-//call out new password similar to showalert function
-
-
-
-//reset settings for new password
+//copy current password to clipboard
