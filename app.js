aa00159950bbb8077be218bdeb1610ff39a88bca
Tom Pocius <poci14@gmail.com>
2019-10-04 13:54:40 -0500

    added value to css to see char #, fiexed copy to clipboard, styled password section

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
+let slider = document.getElementById("password-slide");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
 
 let basicPassword = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']; 
 const numberGen = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-const specialGen = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '+', '=', '{', '[', '}', ']', '<', '>', '?', '+'];
+const specialGen = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '+', '=', '{', '[', '}', ']', '?', '+'];
 const capLetterGen = ['Q', 'W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
 
 function generatePassword(){
   let tempPassword = basicPassword
-  if (document.getElementById("numberSwitch").checked){
+  if (document.getElementById("number-switch").checked){
     tempPassword = tempPassword.concat(numberGen);
   };
   
-  if (document.getElementById("symbolSwitch").checked){
+  if (document.getElementById("symbol-switch").checked){
     tempPassword = tempPassword.concat(specialGen);
   };
 
-  if (document.getElementById("capitalizationSwitch").checked){
+  if (document.getElementById("capitalization-switch").checked){
     tempPassword = tempPassword.concat(capLetterGen);
   };
     pw ='';
-    for (i = 0; i < document.getElementById("passwordSlide").value; i++){
+    for (i = 0; i < document.getElementById("password-slide").value; i++){
       //select random specailgen
       pw = pw.concat(tempPassword[Math.floor(Math.random() * tempPassword.length)]);
     };
-    document.getElementById('pwBox').innerHTML = pw;
+    document.getElementById('pw-box').innerHTML = pw;
+    console.log(pw.length);
+    console.log(pw);
   };
 
-document.getElementById('genPw').addEventListener('click', generatePassword);
+document.getElementById('gen-pw').addEventListener('click', generatePassword);
 
 //copy current password to clipboard
 
 function copyClipboard() {
-  var elm = document.getElementById('pwBox');
+  let elm = document.getElementById('pw-box');
   // for Internet Explorer
 
   if (document.body.createTextRange) {
-    var range = document.body.createTextRange();
+    let range = document.body.createTextRange();
     range.moveToElementText(elm);
     range.select();
     document.execCommand('Copy');
@@ -43,8 +52,8 @@ function copyClipboard() {
   } else if (window.getSelection) {
     // other browsers
 
-    var selection = window.getSelection();
-    var range = document.createRange();
+    let selection = window.getSelection();
+    let range = document.createRange();
     range.selectNodeContents(elm);
     selection.removeAllRanges();
     selection.addRange(range);
@@ -53,4 +62,4 @@ function copyClipboard() {
   }
 }
 
-document.getElementById('copyPass').addEventListener('click', copyClipboard);
+document.getElementById('copy-pass').addEventListener('click', copyClipboard);
